773b457e04f4cccc93463beb7271d7c521978ae0
PiotrPawlak <piotr.pawlak@pl.ibm.com>
2018-11-23 22:24:08 +0100

    History interface added in Model/Core folder

--- Calc/Forms/Calc/Calc.cs
+++ Calc/Forms/Calc/Calc.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Calc
 {
     public partial class CalcForm : Form
---
+++ Calc/Model/Core/ICoreHistory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc.Model.Core
+{
+    interface ICoreHistory
+    {
+        void AddToHistory<T>(T value);
+        List<KeyValuePair<string, DateTime>> GetHistory();
+        void ClearHistory(List<KeyValuePair<string, DateTime>> history);
+    }
+}
